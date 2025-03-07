2972c2e3c71718ccd1a956e1970ef4744dd846ba
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-03-06 23:48:01 -0600

    Parcial05 - Add sort by to the SPA

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -6,12 +6,13 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService } from '../../_services/account.service';
 import { UserParams } from '../../_models/userParams';
 import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule, FormsModule],
-    templateUrl: './member-list.component.html',
+  imports: [MemberCardComponent, PaginationModule, FormsModule, ButtonsModule],
+  templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
