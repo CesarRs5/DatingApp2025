69ac25ad1ccb978bc3af7dca83b2b4af2688a7b1
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-03-12 13:34:15 -0600

    Parcial05 - Add Like feature in SPA

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,14 +3,17 @@ import { inject, Injectable, signal } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likesService = inject(LikesService);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
+  
 
   login(model: any): Observable<User | void> {
     return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
@@ -36,6 +39,7 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem("user", JSON.stringify(user));
     this.currentUser.set(user);
+    this.likesService.getLikeIds();
   }
 
   logout(): void {
