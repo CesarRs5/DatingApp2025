0666a9906a5ce98d06a6000136d583db346b04cd
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-03-21 12:04:14 -0600

    Parcial05 - Add Get Messages for User

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,11 +1,14 @@
 namespace API.Controllers;
+
 using System.Globalization;
 using API.Data;
 using API.DataEntities;
 using API.DTOs;
 using API.Extensions;
+using API.Helpers;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+
 public class MessagesController
     (IMessageRepository messageRepository,
     IUserRepository userRepository,
@@ -15,16 +18,20 @@ public class MessagesController
     public async Task<ActionResult<MessageResponse>> CreateMessage(MessageRequest request)
     {
         var username = User.GetUserName();
+
         if (username == request.RecipientUsername.ToLower(CultureInfo.InvariantCulture))
         {
             return BadRequest("You can't message yourself!");
         }
+
         var sender = await userRepository.GetByUsernameAsync(username);
         var recipient = await userRepository.GetByUsernameAsync(request.RecipientUsername);
+
         if (recipient == null || sender == null)
         {
             return BadRequest("The message can't be sent right now");
         }
+
         var message = new Message
         {
             Sender = sender,
@@ -33,11 +40,24 @@ public class MessagesController
             RecipientUsername = recipient.UserName,
             Content = request.Content
         };
+
         messageRepository.Add(message);
+
         if (await messageRepository.SaveAllAsync())
         {
             return Ok(mapper.Map<MessageResponse>(message));
         }
+
         return BadRequest("Something went wrong!");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageResponse>>> GetMessagesForUser(
+        [FromQuery] MessageParams messageParams)
+    {
+        messageParams.Username = User.GetUserName();
+        var messages = await messageRepository.GetForUserAsync(messageParams);
+        Response.AddPaginationHeader(messages);
+        return messages;
     }
 }
