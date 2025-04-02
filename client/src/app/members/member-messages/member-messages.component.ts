c7a58ef5d5975dd882833c09e61af384e416c335
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-04-02 14:24:26 -0600

    Parcial05 - Add Activated Tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -13,15 +13,11 @@ import { TimeagoModule } from 'ngx-timeago';
 export class MemberMessagesComponent implements OnInit {
   private messagesService = inject(MessagesService);
   username = input.required<string>();
-  messages: Message[] = [];
+  messages = input.required<Message[]>();
 
   ngOnInit(): void {
-    this.loadMessages();
+   
   }
 
-  loadMessages() {
-    this.messagesService.getMessageThread(this.username()).subscribe({
-      next: messages => this.messages = messages
-    });
-  }
+  
 }
