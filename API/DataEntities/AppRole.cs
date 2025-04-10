8e8bb5b862465d3d8d9b88ff7fae088ca2e3d868
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-04-10 13:38:34 -0600

    parcial06 - Add identity entities

---
+++ API/DataEntities/AppRole.cs
@@ -0,0 +1,9 @@
+
+namespace API.DataEntities;
+
+using Microsoft.AspNetCore.Identity;
+
+public class AppRole : IdentityRole<int>
+{
+    public ICollection<AppUserRole> UserRoles { get; set; } = [];
+}
