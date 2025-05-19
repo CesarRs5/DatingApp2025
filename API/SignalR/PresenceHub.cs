dac5d379e17f1bc8623baa92d784de8f4ecf8c5e
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-05-19 17:07:58 -0600

    Parcial06 - Fix Presence Tracker with SignalR

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -13,6 +13,8 @@ public class PresenceHub(PresenceTracker presenceTracker) : Hub
         {
             throw new HubException("Cannot get the current user claim");
         }
+        await presenceTracker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
+        await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserName());
 
         await GetOnlineUsers();
 
