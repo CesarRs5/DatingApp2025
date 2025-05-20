a8597f361e209e231ca15059d1344e5b9bff6938
cesar.rs.5 <cesar.rs.5@gmail.com>
2025-05-20 16:55:46 -0600

    Parcial06 - Add sending messages using SugnalR Hub

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -31,6 +31,9 @@ export class MessagesService {
     this.hubConnection.on("ReceiveMessageThread", messages => {
       this.messageThread.set(messages);
     });
+    this.hubConnection.on("NewMessage", message => {
+      this.messageThread.update(messages => [...messages, message]);
+    })
   }
 
   stopHbuConnection() {
@@ -53,8 +56,8 @@ export class MessagesService {
     return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);  
   }
   
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection?.invoke("SendMessage", { recipientUsername: username, content});
   }
 
   deleteMessage(id: number) {
